deca20e6b952891ccb1047f4beea28ae762c4e51
JosephKevinMachado <joseph.machado.professional@gmail.com>
2024-09-25 08:02:41 -0400

    2024-09-25-08-02-41

--- dags/tests/unit/test_dim_customer.py
+++ dags/tests/unit/test_dim_customer.py
@@ -1,7 +1,7 @@
 import pytest
 import polars as pl
 from polars.testing import assert_frame_equal
-from de_project.etl.silver import dim_customer
+from etl.silver import dim_customer
 
 
 # Sample data for testing
